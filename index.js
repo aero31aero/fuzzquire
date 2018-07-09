23041729f122aee3ffcb5588fbb2def4dc578665
Rohitt Vashishtha <aero31aero@gmail.com>
2018-07-09 22:47:31 +0530

    Add support for multiple extensions, bump release to 1.0.0.
    This should be stable for quite some time now, and it is
    usable as it is.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var projectroot = require('project-root-path');
 var walkSync = require('walk-sync');
+var Path = require('path');
 
 var isMatch = function(path, trypath){
     var counter = 0;
     var pathparts = path.split('/');
     var tryparts = trypath.split('/');
-    for(j=0;j<tryparts.length; j++){
+    for (j=0; j<tryparts.length; j++) {
         var elem1 = tryparts[j];
         var index = pathparts.indexOf(elem1);
         if (! (index >= counter)) return false;
@@ -13,9 +14,27 @@ var isMatch = function(path, trypath){
     return true;
 }
 
-var paths = [];
+var logging = true; // default
 
-var loader = function(projectroot, logging = false) {
+var getPathIfMatches = function (checkpath, path, modulepath, elem) {
+    if(logging) console.log("Fuzzquire: Searching for:", path);
+    if(isMatch(checkpath, path)){
+        if(logging) console.log("Fuzzquire: Found:", elem);
+        if(!modulepath){
+            modulepath = elem;
+        }
+        else {
+            throw new Error('Fuzzquire: Ambiguous Module Path: ' + path);
+        }
+    }
+    return modulepath;
+}
+
+var paths = [];
+var exts = ['.js', '.json'];
+
+var loader = function(projectroot, enable_logging = false) {
+    logging = enable_logging;
     if (!process.env.FUZZQUIRE_DATA) {
         if(logging) console.log("Fuzzquire: Reading paths into memory.");
         paths = walkSync(projectroot, {
@@ -29,31 +48,33 @@ var loader = function(projectroot, logging = false) {
     }
 }
 
-var fuzzquire = function(path, logging = false){
+var fuzzquire = function(path){
     var module = null;
     loader(projectroot, logging);
     var modulepath = false;
     var elem = null;
-    if(!path.endsWith('.js')) path +='.js';
+    const loopoverext = Path.extname(path) == '' ? true : false;
     for(i=0;i<paths.length;i++){
         elem = paths[i];
-        var checkpath = elem
+        var checkpath = elem;
         if(logging) console.log("Fuzzquire: Traversing:",elem);
         if(elem.endsWith('/index.js')) {
             checkpath = elem.replace('/index.js', '.js');
         }
-        if(isMatch(checkpath, path)){
-            if(logging) console.log("Fuzzquire: Found:",elem)
-            if(!modulepath){
-                modulepath = elem;
-            }
-            else {
-                throw new Error('Fuzzquire: Ambiguous Module Path: ' + path);
-            }
+        if (loopoverext) {
+            exts.forEach((ext) => {
+                modulepath = getPathIfMatches(checkpath, path + ext, modulepath, elem);
+            });
+        } else {
+            modulepath = getPathIfMatches(checkpath, path, modulepath, elem);
         }
+
     }
     try {
-        if(logging) console.log("Fuzzquire: Loading:",projectroot + "/" + modulepath);
+        if (!modulepath) {
+            throw new Error('Fuzzquire: Couldn\'t find specified module.');
+        }
+        if (logging) console.log("Fuzzquire: Loading:", projectroot + "/" + modulepath);
         module = require(projectroot + "/" + modulepath);
         return module;
     }
